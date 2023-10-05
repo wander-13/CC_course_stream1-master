561120965d0fb8418d7013748d8f6f7bb57288f0
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-05 12:21:07 -0400

    Practice Challenge - attempt challenge - check solutions

--- 04_Data_manip_1/Intro_data_manip_R.R
+++ 04_Data_manip_1/Intro_data_manip_R.R
@@ -185,3 +185,52 @@ experiment2 <- merge(elongation_long, treatments, by.x = c("zone", "indiv"), by.
 # same result!
 
 boxplot(length ~ Treatment, data = experiment)
+
+### Challenge
+
+## Load data
+dragons <- read.csv('04_Data_manip_1/dragons.csv', header = TRUE)
+
+## Clean the dataset
+# Change paprika to turmeric
+dragons <- rename(dragons, turmeric = paprika)
+
+
+# Fix the calibration error for tabasco by horntail
+correct.values  <- dragons$tabasco[dragons$species == 'hungarian_horntail'] - 30   # create a vector of corrected values
+dragons[dragons$species == 'hungarian_horntail', 'tabasco'] <- correct.values      # overwrite the values in the dragons object
+
+# OR
+
+dragons.2 <- mutate(dragons, tabasco = ifelse(species == 'hungarian_horntail', tabasco - 30, tabasco))
+# This creates (overwrites) the column tabasco using the following logic: if the species is Hungarian Horntail, 
+# deduct 30 from the values in the (original) tabasco column; if the species is NOT horntail 
+# (i.e. all other species), write the original values.
+
+# Reshape the data from wide to long format
+dragons_long <- gather(dragons, key = 'spice', value = 'plume', c('tabasco', 'jalapeno', 'wasabi', 'turmeric'))
+
+# Convert the data into meters
+dragons_long <- mutate(dragons_long, plume.m = plume/100)    # Creating a new column turning cm into m
+
+# Create a subset for each species to make boxplots
+horntail <- filter(dragons_long, species == 'hungarian_horntail')            # the dplyr way of filtering
+green <- filter(dragons_long, species == 'welsh_green')
+shortsnout <- dragons_long[dragons_long$species == 'swedish_shortsnout', ]   # maybe you opted for a base R solution instead?
+
+# Make the boxplots
+par(mfrow=c(1, 3))      # you need not have used this, but it splits your plotting device into 3 columns where the plots will appear, so all the plots will be side by side.
+
+boxplot(plume.m ~ spice, data = horntail,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Hungarian Horntail')
+
+boxplot(plume.m ~ spice, data = green,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Welsh Green')
+
+boxplot(plume.m ~ spice, data = shortsnout,
+        xlab = 'Spice', ylab = 'Length of fire plume (m)',
+        main = 'Swedish Shortsnout')
+
+
