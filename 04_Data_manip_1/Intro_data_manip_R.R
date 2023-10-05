9fa23619a7c375e01c75bf43237218fec74a74fb
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-05 08:55:54 -0400

    Add code to intro_data_manip_R file - explore results Add to notes file - operators for logical operations

--- 04_Data_manip_1/Intro_data_manip_R.R
+++ 04_Data_manip_1/Intro_data_manip_R.R
@@ -8,7 +8,7 @@ setwd()
 # it is already set in the R project folder/repo
 
 # Load the elongation data
-elongation <- read.csv("EmpetrumElongation.csv", header = TRUE)   
+elongation <- read.csv("04_Data_manip_1/EmpetrumElongation.csv", header = TRUE)   
 
 # Check import and preview data
 head(elongation)   # first few observations
@@ -27,3 +27,21 @@ elongation[6, ]
 # And of course you can mix it all together!
 elongation[6, ]$Indiv   # returns the value in the column Indiv for the sixth observation
 # (much easier calling columns by their names than figuring out where they are!)
+
+# Let's access the values for Individual number 603
+elongation[elongation$Indiv == 603, ]
+
+
+# Subsetting with one condition
+
+elongation[elongation$Zone < 4, ]    # returns only the data for zones 2-3
+elongation[elongation$Zone <= 4, ]   # returns only the data for zones 2-3-4
+
+
+# This is completely equivalent to the last statement
+elongation[!elongation$Zone >= 5, ]   # the ! means exclude
+
+
+# Subsetting with two conditions
+elongation[elongation$Zone == 2 | elongation$Zone == 7, ]    # returns only data for zones 2 and 7
+elongation[elongation$Zone == 2 & elongation$Indiv %in% c(300:400), ]    # returns data for shrubs in zone 2 whose ID numbers are between 300 and 400
