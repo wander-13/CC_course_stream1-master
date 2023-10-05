46744cff5faf80ca3b5d26961cd37d56ad90ba1c
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-05 09:11:13 -0400

    Added to Notes document - useful sequence builders Added to intro_data_manip_R document - CHANGING VARIABLE NAMES AND VALUES IN A DATA FRAME

--- 04_Data_manip_1/Intro_data_manip_R.R
+++ 04_Data_manip_1/Intro_data_manip_R.R
@@ -45,3 +45,42 @@ elongation[!elongation$Zone >= 5, ]   # the ! means exclude
 # Subsetting with two conditions
 elongation[elongation$Zone == 2 | elongation$Zone == 7, ]    # returns only data for zones 2 and 7
 elongation[elongation$Zone == 2 & elongation$Indiv %in% c(300:400), ]    # returns data for shrubs in zone 2 whose ID numbers are between 300 and 400
+
+## CHANGING VARIABLE NAMES AND VALUES IN A DATA FRAME
+
+# Let's create a working copy of our object
+elong2 <- elongation
+
+# Now suppose you want to change the name of a column: you can use the names() function
+# Used on its own, it returns a vector of the names of the columns. Used on the left side of the assign arrow, it overwrites all or some of the names to value(s) of your choice.
+
+names(elong2)                 # returns the names of the columns
+
+names(elong2)[1] <- "zone"    # Changing Zone to zone: we call the 1st element of the names vector using brackets, and assign it a new value
+names(elong2)[2] <- "ID"      # Changing Indiv to ID: we call the 2nd element and assign it the desired value
+
+# Now suppose there's a mistake in the data, and the value 5.1 for individual 373 in year 2008 should really be 5.7
+
+## - option 1: you can use row and column number
+elong2[1,4] <- 5.7
+
+## - option 2: you can use logical conditions for more control
+elong2[elong2$ID == 373, ]$X2008 <- 5.7   # completely equivalent to option 1
+
+## CREATING A FACTOR
+
+# Let's check the classes
+str(elong2)
+
+# The zone column shows as integer data (whole numbers), but it's really a grouping factor (the zones could have been called A, B, C, etc.) Let's turn it into a factor:
+
+elong2$zone <- as.factor(elong2$zone)        # converting and overwriting original class
+str(elong2)                                  # now zone is a factor with 6 levels
+
+## CHANGING A FACTOR'S LEVELS
+
+levels(elong2$zone)  # shows the different factor levels
+
+levels(elong2$zone) <- c("A", "B", "C", "D", "E", "F")   # you can overwrite the original levels with new names
+
+# You must make sure that you have a vector the same length as the number of factors, and pay attention to the order in which they appear!
