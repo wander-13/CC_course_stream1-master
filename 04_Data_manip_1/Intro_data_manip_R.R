89e34b1767c4a17157f82f8d004155ee38ad2c85
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-05 09:41:26 -0400

    Added to Intro_data_manip_R - section 2 and 3  - explore differences in data manipulations

--- 04_Data_manip_1/Intro_data_manip_R.R
+++ 04_Data_manip_1/Intro_data_manip_R.R
@@ -2,6 +2,7 @@
 # Author: Elin Swank
 # Date: 10/05/2023
 
+# 1. SUBSET, EXTRACT, AND MODIFY DATA WITH R OPERATORS
 
 # Set your working directory to where the folder is saved on your computer
 setwd()
@@ -84,3 +85,87 @@ levels(elong2$zone)  # shows the different factor levels
 levels(elong2$zone) <- c("A", "B", "C", "D", "E", "F")   # you can overwrite the original levels with new names
 
 # You must make sure that you have a vector the same length as the number of factors, and pay attention to the order in which they appear!
+
+# 2. WHAT IS TIDY DATA AND HOW DO WE ACHIEVE IT
+
+install.packages("tidyr")  # install the package
+library(tidyr)             # load the package
+
+
+elongation_long <- gather(elongation, Year, Length,                           # in this order: data frame, key, value
+                          c(X2007, X2008, X2009, X2010, X2011, X2012))        # we need to specify which columns to gather
+
+# Here we want the lengths (value) to be gathered by year (key)
+
+# Let's reverse! spread() is the inverse function, allowing you to go from long to wide format
+elongation_wide <- spread(elongation_long, Year, Length)
+
+elongation_long2 <- gather(elongation, Year, Length, c(3:8))
+
+boxplot(Length ~ Year, data = elongation_long,
+        xlab = "Year", ylab = "Elongation (cm)",
+        main = "Annual growth of Empetrum hermaphroditum")
+
+# 3. EXPLORE THE MOST COMMON AND USEFUL FUNCTIONS OF dplyr
+
+install.packages("dplyr")  # install the package
+library(dplyr)              # load the package
+
+# 3.a. rename() VARIABLES
+
+elongation_long <- rename(elongation_long, zone = Zone, indiv = Indiv, year = Year, length = Length)     # changes the names of the columns (getting rid of capital letters) and overwriting our data frame
+
+# As we saw earlier, the base R equivalent would have been
+names(elongation_long) <- c("zone", "indiv", "year", "length")
+
+# 3.b. filter() ROWS AND select() COLUMNS
+
+# FILTER OBSERVATIONS
+
+# Let's keep observations from zones 2 and 3 only, and from years 2009 to 2011
+
+elong_subset <- filter(elongation_long, zone %in% c(2, 3), year %in% c("X2009", "X2010", "X2011")) # you can use multiple different conditions separated by commas
+
+# For comparison, the base R equivalent would be (not assigned to an object here):
+elongation_long[elongation_long$zone %in% c(2,3) & elongation_long$year %in% c("X2009", "X2010", "X2011"), ]
+# NOTE: using %in% here to match a list of exact (character) values.
+
+# SELECT COLUMNS
+
+# Let's ditch the zone column just as an example
+
+elong_no.zone <- dplyr::select(elongation_long, indiv, year, length)   # or alternatively
+elong_no.zone <- dplyr::select(elongation_long, -zone) # the minus sign removes the column
+
+# For comparison, the base R equivalent would be (not assigned to an object here):
+elongation_long[ , -1]  # removes first column
+
+# A nice hack! select() lets you rename and reorder columns on the fly
+elong_no.zone <- dplyr::select(elongation_long, Year = year, Shrub.ID = indiv, Growth = length)
+
+# Neat, uh?
+
+# 3.c. mutate() YOUR DATASET BY CREATING NEW COLUMNS
+
+# CREATE A NEW COLUMN
+
+elong_total <- mutate(elongation, total.growth = X2007 + X2008 + X2009 + X2010 + X2011 + X2012)
+
+# 3.d. groupby() CERTAIN FACTORS TO PERFORM OPERATIONS ON CHUNKS OF DATA
+
+# GROUP DATA
+
+elong_grouped <- group_by(elongation_long, indiv)   # grouping our dataset by individual
+
+# 3.e. summarise() DATA WITH A RANGE OF SUMMARY STATISTICS
+
+# SUMMARISING OUR DATA
+
+summary1 <- summarise(elongation_long, total.growth = sum(length))
+summary2 <- summarise(elong_grouped, total.growth = sum(length))
+
+summary3 <- summarise(elong_grouped, total.growth = sum(length),
+                      mean.growth = mean(length),
+                      sd.growth = sd(length))
+
+# 6. ..._join() DATASETS BASED ON SHARED ATTRIBUTES
