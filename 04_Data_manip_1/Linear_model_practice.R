4f48d1c6e2dbd4b8cf00b1b1daadc3434228496d
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-06 07:43:39 -0400

    Linear_model_practice - create theme function - factorize spacing variables - create boxplot for apples data    - x = spacing, y = yield

--- 04_Data_manip_1/Linear_model_practice.R
+++ 04_Data_manip_1/Linear_model_practice.R
@@ -11,3 +11,29 @@ library(agridat)
 apples <- agridat::archbold.apple
 head(apples)
 summary(apples)
+
+theme.clean <- function(){
+  theme_bw()+
+    theme(axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust = 1),
+          axis.text.y = element_text(size = 12),
+          axis.title.x = element_text(size = 14, face = "plain"),             
+          axis.title.y = element_text(size = 14, face = "plain"),             
+          panel.grid.major.x = element_blank(),                                          
+          panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.y = element_blank(),  
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = , "cm"),
+          plot.title = element_text(size = 20, vjust = 1, hjust = 0.5),
+          legend.text = element_text(size = 12, face = "italic"),          
+          legend.position = "right")
+}
+
+apples$spacing2 <- as.factor(apples$spacing)
+
+library(ggplot2)
+
+(apples.p <- ggplot(apples, aes(spacing2, yield)) +
+    geom_boxplot(fill = "#CD3333", alpha = 0.8, colour = "#8B2323") +
+    theme.clean() +  
+    theme(axis.text.x = element_text(size = 12, angle = 0)) +
+    labs(x = "Spacing (m)", y = "Yield (kg)"))
