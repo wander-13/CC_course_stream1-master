2b597c4457d24d4836a983c8b4fd3655e8964afc
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-06 12:43:46 -0400

    Create R script for data visualization practice - title, author, date, email

---
+++ 06_Data_vis_1/data_vis_practice.R
@@ -0,0 +1,8 @@
+# AESTHETIC AND INFORMATIVE DATA VISUALIZATION:
+# USING GGPLOT2 TO COMMUNICATE YOUR RESULTS
+# AUTHOR: ELIN SWANK
+# 10/06/2023
+# elinswank@gmail.com
+
+################################################################################
+# MAKING DIFFERENT PLOTS WITH GGPLOT2
